4d9125d9b7c611fd48814501b1948e75f504e6d7
Philippe SERRAT <pse@mustinformatique.fr>
2018-10-11 12:32:02 +0200

    Not an elegant fix but job is done

--- omnisStudioIndenter.py
+++ omnisStudioIndenter.py
@@ -5,7 +5,7 @@ import re
 
 class OmnisstudioindenterCommand(sublime_plugin.TextCommand):
 
-    LEVEL_UPS = ['For', 'If', 'Else', 'While', 'Switch', 'Begin', 'Repeat']
+    LEVEL_UPS = ['For', 'If', 'While', 'Switch', 'Begin', 'Repeat']
     LEVEL_DOWNS = ['End', 'Until']
 
     def getIndentLevel(self, codeStatement, level):
@@ -43,46 +43,54 @@ class OmnisstudioindenterCommand(sublime_plugin.TextCommand):
         for codeLine in codeList:
             indention = indentionSpace * level
             codeStatementMatch = re.match('^\w+', codeLine)
+
             if codeStatementMatch:
                 codeStatement = codeStatementMatch.group()
                 level = self.getIndentLevel(codeStatement, level)
-                if codeStatement in self.LEVEL_DOWNS:
-                    indention = indentionSpace * level
+                
+                if codeStatement == 'Else':
+                    indention = indentionSpace * (level - 1)
+                    indentedCode = indentedCode + indention + codeLine
+                else:
+                    if codeStatement in self.LEVEL_DOWNS:
+                        indention = indentionSpace * level
 
-                if codeStatement in specialStatements:
-                    specialStatement = True
-                    specialLevel = level - 1
-                    if level == 0:
-                        specialSubLevel = 1
-                    else:
-                        specialSubLevel = specialLevel + 1
+                    if codeStatement in specialStatements:
+                        specialStatement = True
+                        specialLevel = level - 1
+                        if level == 0:
+                            specialSubLevel = 1
+                        else:
+                            specialSubLevel = specialLevel + 1
 
-                elif codeStatement in specialSubStatement:
-                    specialSubLevel = specialLevel + 2
-                    indention = indentionSpace * (specialLevel + 1)
+                    elif codeStatement in specialSubStatement:
+                        specialSubLevel = specialLevel + 2
+                        indention = indentionSpace * (specialLevel + 1)
 
-                elif specialStatement and codeStatement not in specialStatements:
-                    if re.match('^End Switch', codeLine):
-                        specialStatement = False
-                        indention = indentionSpace * specialLevel
-
-                    else:
-                        if codeStatement in self.LEVEL_DOWNS:
-                            specialSubLevel = self.getIndentLevel(
-                                codeStatement, specialSubLevel)
-                            indention = indentionSpace * specialSubLevel
-
-                        elif codeStatement in self.LEVEL_UPS:
-                            indention = indentionSpace * specialSubLevel
-                            specialSubLevel = self.getIndentLevel(
-                                codeStatement, specialSubLevel)
+                    elif specialStatement and codeStatement not in specialStatements:
+                        if re.match('^End Switch', codeLine):
+                            specialStatement = False
+                            indention = indentionSpace * specialLevel
 
                         else:
-                            indention = indentionSpace * \
-                                self.getIndentLevel(
+                            if codeStatement in self.LEVEL_DOWNS:
+                                specialSubLevel = self.getIndentLevel(
+                                    codeStatement, specialSubLevel)
+                                indention = indentionSpace * specialSubLevel
+
+                            elif codeStatement in self.LEVEL_UPS:
+                                indention = indentionSpace * specialSubLevel
+                                specialSubLevel = self.getIndentLevel(
                                     codeStatement, specialSubLevel)
 
-            indentedCode = indentedCode + indention + codeLine
+                            else:
+                                indention = indentionSpace * \
+                                    self.getIndentLevel(
+                                        codeStatement, specialSubLevel)
+            
+                    indentedCode = indentedCode + indention + codeLine
+            else:
+                indentedCode = indentedCode + indention + codeLine
         return indentedCode
 
     def run(self, edit):
